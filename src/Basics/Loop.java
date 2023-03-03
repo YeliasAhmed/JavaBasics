3c54db82029974745423634b764e7bc8a472a628
Yelias Ahmed <yeliasahmed89@gmail.com>
2023-03-03 12:10:45 +0600

    Updated files

--- src/Basics/Loop.java
+++ src/Basics/Loop.java
@@ -1,11 +1,10 @@
+<<<<<<< HEAD
 package Basics;
 
 public class Loop {
     public static void main(String[] args) {
 
-        int result = sum(5,10);
-        System.out.println("This is method:");
-        System.out.println(result);
+
 // for loop example start
         int i = 1; // i++ = i+1=2+1=3
 
@@ -37,3 +36,39 @@ public class Loop {
 
 
 
+=======
+package Basics;
+
+public class Loop {
+    public static void main(String[] args) {
+
+
+// for loop example start
+        int i = 1; // i++ = i+1=2+1=3
+
+        System.out.println("This is for loop");
+        for(i=1;i<5;i++)
+        {
+            System.out.println(i); // output: 1, 2, 3,4
+        }
+        //    for loop end
+
+        //    while loop start
+
+        int j= 1;
+        System.out.println("That is while loop");
+        while (j<5){
+            System.out.println(j);
+            j++;
+        }
+
+        }
+
+
+ }
+
+
+
+
+
+>>>>>>> 15883a4 (Updated OOP)
--- src/OOP/InputDataType.java
+++ src/OOP/InputDataType.java
@@ -44,3 +44,4 @@ public class InputDataType {
         }
     }
 }
+>>>>>>> 15883a4 (Updated OOP)
